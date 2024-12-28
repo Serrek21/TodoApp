e3731983753ea2f20aa9866f141b21232966209d
Stepa_fd79 <Stepa_fd79@student.itstep.org>
2024-12-28 11:03:41 +0200

    Добавьте файлы проекта.

---
+++ Data/TodoList.cs
@@ -0,0 +1,34 @@
+﻿namespace TodoApp.Data;
+
+public static class TodoList
+{
+    private static List<Todo> todos = new();
+
+    public static Todo Add(string title)
+    {
+        var newTodo = new Todo
+        {
+            Title = title,
+            IsCompleted = false,
+            Id = Guid.NewGuid()
+        };
+        todos.Add(newTodo);
+        return newTodo;
+    }
+
+    public static void Delete(Guid id)
+    {
+        todos.RemoveAll(t => t.Id == id);
+    }
+
+    public static void Complete(Guid id)
+    {
+        Todo todo = todos.First(t => t.Id == id);
+        todo.IsCompleted = true;
+    }
+
+    public static void Clear()
+    {
+        todos.Clear();
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using TodoApp.Components;
+using TodoApp.Data;
+
+namespace TodoApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    // Функція для додавання нової задачі
+    public void OnAddClick(object sender, RoutedEventArgs e)
+    {
+        string title = TitleTextBox.Text;
+        Todo newTodo = TodoList.Add(title);
+        TodoComponent component = new TodoComponent(newTodo);
+        TodoListPanel.Children.Add(component);
+    }
+
+    // Функція для очищення списку задач
+    public void OnClearClick(object sender, RoutedEventArgs e)
+    {
+        TodoList.Clear();              // Очищує логічний список задач
+        TodoListPanel.Children.Clear(); // Очищує візуальні компоненти
+    }
+}
